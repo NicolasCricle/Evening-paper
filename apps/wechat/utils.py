568a6f435e89fffc05d243550f6b6cf7b7a45c2e
zhangjinlin <17625905485@163.com>
2019-12-05 16:10:10 +0800

    微信测试回复4

--- apps/wechat/utils.py
+++ apps/wechat/utils.py
@@ -1,6 +1,8 @@
 import time
 import xmltodict
 
+from flask import current_app
+
 
 class WechatMessage(object):
     
@@ -17,6 +19,7 @@ class ReplyMessage(WechatMessage):
     def __init__(self, request):
         super().__init__(request)
         self._xmlStr = request.stream.read()
+        current_app.logger.info(self._xmlStr)
         self.init_xml()
         self._xml = None
 
@@ -40,8 +43,14 @@ class ReplyMessage(WechatMessage):
                     f'<CreateTime>{str(int(time.time()))}</CreateTime>'
 
         self._xml += f'<MsgType><![CDATA[text]]></MsgType>'\
-                     f'<Content><![CDATA[{value}]]></Content>'
+                     f'<Content><![CDATA[{value}]]></Content></xml>'
+
+
+def main():
+    xml = """<xml><ToUserName><![CDATA[gh_7c6b36e42ebc]]></ToUserName>\n<FromUserName><![CDATA[oORd9t3JHxveUhZgReoyQFWCpFro]]></FromUserName>\n<CreateTime>1575529721</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[1234]]></Content>\n<MsgId>22556159405738233</MsgId>\n</xml>"""
+    print(str(xml))
+    print(str(xmltodict.parse(xml)))
 
 
 if __name__ == "__main__":
-    pass
+    main()
